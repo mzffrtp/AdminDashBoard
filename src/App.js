fcd3ad8d02dd0772fbe87cda15cf16c6a25ee0ac
mzffrtp <mzffr.tpgl@gmail.com>
2023-08-14 00:34:27 +0200

    sidebar component
    locig

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { FiSettings } from "react-icons/fi"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+import Sidebar from "./components/Sidebar";
 function App() {
   const activeMenu = true
   return (
     <BrowserRouter>
-      <div className="flex relative dark:bg-main-dark-bg">
+      <div className="flex relative dark:bg-main-dark-bg mt-28">
         <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
           <TooltipComponent content="Settings" position="Top">
             <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray" style={{ borderRadius: "50%", backgroundColor: "pink" }}>
@@ -17,9 +18,9 @@ function App() {
         </div>
         {
           activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white"> side bar </div>
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white mt-28"><Sidebar /></div>
           ) : (
-            <div className="w-0 dark:bg-secondary-dark-bg">side bar X</div>
+            <div className="w-0 dark:bg-secondary-dark-bg"><Sidebar /></div>
           )}
         <div className={`dark:bg-main-drk-bg bg-main-bg min-h-screen w-full border-4 border-red-600 ${activeMenu ? "md:ml-72" : "flex-2"}`}>
           <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">navbar</div>
