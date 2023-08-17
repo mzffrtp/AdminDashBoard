d82aa3f71097c6281fe628439fb756ab2250c825
mzffrtp <mzffr.tpgl@gmail.com>
2023-08-17 23:40:28 +0200

    ecommerce component
    JNK%3456 bug responsive design >then 900

--- src/pages/Ecommerce/ecommerce-components/Reveneu.jsx
+++ src/pages/Ecommerce/ecommerce-components/Reveneu.jsx
@@ -1,26 +1,36 @@
 import React from "react"
 import { Button, SparkLine, Stacked } from "../../../components"
 import { SparklineAreaData } from "../../../data/dummy"
+import { useStateContext } from "../../../context/ContextProvider"
 
 export default function Reveneu () {
+    const {screenSize} = useStateContext() 
     return (
         <div className="flex gap-10 flex-wrap justify-center">
-            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4  rounded-2xl md:w-70">
-                <div className="flex justify-between gap-3">
-                    <p className="font-semibold text-xl">Revenue Updates</p>
-                    <div className="flex items-center gap-4">
-                        <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
-                            <span>.</span>
-                            <span>Expense</span>
-                        </p>
-                        <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
-                            <span>.</span>
-                            <span>Budget</span>
-                         </p>
-                    </div>
-                </div>
+            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-70">
+                {
+                    screenSize<900 ? (
+                        <div className="flex justify-center">
+                            <p className="font-semibold text-xl">Revenue Updates</p>
+                        </div>
+                    ) : (
+                            <div className="flex justify-between gap-3">
+                                <p className="font-semibold text-xl">Revenue Updates</p>
+                                <div className="flex items-center gap-4">
+                                    <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                                        <span>.</span>
+                                        <span>Expense</span>
+                                    </p>
+                                    <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                                        <span>.</span>
+                                        <span>Budget</span>
+                                    </p>
+                                </div>
+                            </div>)
+                }
+                
                 <div className="mt-10 flex gap-10 flex-wrap justify-center">
-                    <div className="border-r-1 border-color m-4 pr-10">
+                    <div className={`border-r-1 border-color m-4 pr-10 ${screenSize<900 && "border-e-0"}`}>
                         <div>
                             <p>
                             <span className="text-3xl font-semibold">$93,438</span>
@@ -52,7 +62,7 @@ export default function Reveneu () {
                             />
                         </div>
                     </div>
-                    <div>
+                    <div className={`${screenSize<900 && "flex justify-center w-3/5"}`}>
                         <Stacked />
                     </div>
                 </div>
