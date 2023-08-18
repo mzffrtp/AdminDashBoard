722efff716afcf5b990e363d99b8cb9943edad3d
mzffrtp <mzffr.tpgl@gmail.com>
2023-08-18 22:05:52 +0200

    emloyees

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,38 +1,29 @@
-import React from 'react'
-import Header from "../components/Header";
-import {GridComponent, ColumnsDirective, ColumnDirective, Inject, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport} from "@syncfusion/ej2-react-grids";
-import {ordersData, ordersGrid, contextMenuItems} from "../data/dummy"
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Sort, ContextMenu, Filter, Page, Resize, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
-function Orders() {
-  const editing = {allowDeletind:true}
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
+
+const Orders = () => {
   return (
-    <div className='md-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
-      <Header  category={"Page"} title={"Orders"}/>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
       <GridComponent
-      id='gridcomp'
-      dataSource={ordersData}
-      allowPaging
-      allowSorting
-      allowExcelExport
-      allowPdfExport
-      contextMenuItems={contextMenuItems}
-      allowEditing
-      editSettings={editing}
-      
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+        allowExcelExport
+        allowPdfExport
+        contextMenuItems={contextMenuItems}
       >
         <ColumnsDirective>
           {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        {
-          ordersGrid.map((i, index)=>(
-            <ColumnDirective key={index} {...i} />
-          ))
-        }
+          {ordersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
         </ColumnsDirective>
-        <Inject services={[Resize, Sort, ContextMenu, Filter, ExcelExport, Edit, PdfExport]} />
+        <Inject services={[ Sort, ContextMenu, Filter, ExcelExport, Edit, PdfExport]} />
       </GridComponent>
     </div>
-    
-  )
-}
-
-export default Orders
+  );
+};
+export default Orders;
